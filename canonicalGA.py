e03d4fe591cc1c7297d4efbdb67d253555465c9c
Choirulriz <achoirul.rizal@gmail.com>
2020-02-28 10:18:56 +0700

    Canonical GA using roulet wheel, cxOnePoint, mutation random idx

--- canonicalGA.py
+++ canonicalGA.py
@@ -2,24 +2,12 @@ import numpy
 import random
 import benchmarkFunction as b
 
-#setting up the DNA of population
-def initial_setup(population):
-    for x in range(len(population[:,0])):
-        # ganjil(1) male; genap(0) female
-        if x%2==1:
-            population[x:x+1,0] = 1.0
-        else:
-            population[x:x+1,0] = 0.0
-        # initialize flag for DNA similarity
-        population[x:x+1,1:3] = 0.0
-    return population
-
 def normalize_parameter(population, x_min, x_max):
-    X = population[:,3:]
+    X = population
     nom = (X-X.min(axis=0))*(x_max-x_min)
     denom = X.max(axis=0) - X.min(axis=0)
     denom[denom==0] = 1
-    population[:,3:] = x_min + nom/denom
+    population = x_min + nom/denom
     return  population
 
 def roulette_select(population, fitnesses, number_of_individuals):
@@ -39,7 +27,7 @@ def roulette_select(population, fitnesses, number_of_individuals):
     return new_population
 
 def cxOnePoint(ind1,ind2):
-    ind1[3],ind2[3] = ind2[3],ind1[3]
+    ind1[0],ind2[1] = ind2[0],ind1[1]
     return ind1,ind2
 
 def crossover(parents, num_individuals,num_of_genes):
@@ -53,7 +41,7 @@ def mutation(offspring_crossover):
     for idx in range(offspring_crossover.shape[0]):
         # The random value to be added to the gene.
         random_value = numpy.random.uniform(-1.0, 1.0, 1)
-        random_idx = random.randint(3,4)
+        random_idx = random.randint(0,1)
         check_limit = offspring_crossover[idx, random_idx] + random_value
         if(check_limit <= upper_limit and check_limit >= lower_limit):
             offspring_crossover[idx, random_idx] = offspring_crossover[idx, random_idx] + random_value
@@ -61,28 +49,27 @@ def mutation(offspring_crossover):
 
 # initialize hyperparameter
 num_generations = 10
-num_individuals = 4
-num_of_genes = 5
+num_individuals = 5
+num_of_genes = 2
 lower_limit = -4
 upper_limit = 4
 
 # Defining the population size.
 population_size = (num_individuals,num_of_genes)
 #Creating the initial population.
-pop = numpy.random.uniform(low=lower_limit, high=upper_limit, size=population_size)
-population = initial_setup(pop)
+population = numpy.random.uniform(low=lower_limit, high=upper_limit, size=population_size)
 
 for generation in range(num_generations):
     print("Generation : ", generation)
-    fitness = b.ackley(population[:,3], population[:,-1])
+    fitness = b.ackley(population[:,0], population[:,1])
     parent_selection = roulette_select(population,fitness,num_individuals)
     offspring_crossover = crossover(parent_selection,num_individuals,num_of_genes)
     offspring_mutation = mutation(offspring_crossover)
-    best_result = numpy.max(b.ackley(population[:,3], population[:,-1]))
+    best_result = numpy.max(b.ackley(population[:,0], population[:,1]))
     print("Best Result : ", best_result)
 
-fitness = b.ackley(population[:,3], population[:,-1])
-best_match_idx = numpy.where(fitness == numpy.max(fitness))
+fitness = b.ackley(population[:,0], population[:,1])
+best_match_idx = numpy.where(fitness == best_result)
 print("Best solution : ", population[best_match_idx, :])
 print("Best solution fitness : ", fitness[best_match_idx])
 print("Best f(x) : ", b.ackley_global_minima(population[best_match_idx, 0],population[best_match_idx, 1]))
---
+++ dum.py
@@ -0,0 +1,8 @@
+import random
+
+for x in range(10):
+    y = random.randint(3,4) 
+    print("========")
+    print("x1: ", y)
+    print("x2: ", y)
+    
